41c37edf3dd199b8bd5ef5e70955d1dfe559964f
Benjamin Warner <benjaminwarner@u.boisestate.edu>
2020-06-15 12:38:40 -0600

    Refresh the screen on exit and during errors.

--- src/main.c
+++ src/main.c
@@ -18,6 +18,9 @@ struct termios original_terminal;
 /*** terminal ***/
 
 void die(const char *s) {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
@@ -67,6 +70,8 @@ void editor_process_keypress() {
 
 	switch (c) {
 		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\x1b[2J", 4);
+			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
 			break;
 	}
